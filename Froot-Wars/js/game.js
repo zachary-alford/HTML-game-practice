3d0f4a75d9a92bf4935d995ebe5e5bca1ab601a7
Zachary Alford <zacharyalford3@gmail.com>
2026-07-14 19:01:43 +0200

    Froot Wars - added level screen

--- Froot-Wars/js/game.js
+++ Froot-Wars/js/game.js
@@ -5,6 +5,9 @@ let game = {
     game.canvas = document.getElementById("gamecanvas");
     game.context = game.canvas.getContext("2d");
 
+    // Initialize objects
+    levels.init();
+
     // Hide all game layers and display the start screen
     game.hideScreens();
     game.showScreen("gamestartscreen");
@@ -32,6 +35,50 @@ let game = {
 
     screen.style.display = "block";
   },
+
+  showLevelScreen: function() {
+    game.hideScreens();
+    game.showScreen("levelselectscreen");
+  },
+};
+
+let levels = {
+  //Level data
+  data: [{ // First level
+    foreground: "desert-foreground",
+    backgroud: "clouds-background",
+    entities: []
+  }, { // Second level
+    foreground: "desert-foreground",
+    background: "clouds-background",
+    entities: []
+  }],
+
+  // Initialize level selection screen
+  init: function() {
+    let levelSelectScreen = document.getElementById("levelselectscreen");
+
+    // An event handler to call
+    let buttonClickHandler = function() {
+      game.hideScreen("levelselectscreen");
+
+      // Level label values are 1, 2. Levels are 0, 1
+      levels.load(this.value - 1);
+    };
+
+    for (let i = 0; i < levels.data.length; i++) {
+      let button = document.createElement("input");
+
+      button.type = "button";
+      button.value = (i + 1); // Level labels are 1, 2
+      button.addEventListener("click", buttonClickHandler);
+
+      levelSelectScreen.appendChild(button);
+    }
+  },
+  // Load all data and images for a specific level
+  load: function(number) {
+  }
 };
 
 // Initialize game once page has fully loaded
